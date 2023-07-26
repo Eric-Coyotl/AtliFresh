63568ae461be78a24012954bcd83af44fc2dd935
eric_cytl <coyotleric@gmail.com>
2023-07-26 01:05:59 -0400

    Initial Commit

---
+++ Atli/AtliApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtliApp.swift
+//  Atli
+//
+//  Created by Eric Coyotl on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AtliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
